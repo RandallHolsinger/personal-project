afca4f08ec4d99e8fae8e76158130070ffeede36
Randall Holsinger <randy@Randalls-MacBook-Pro.local>
2019-03-25 12:41:11 -0600

    fixed product component not rendering

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,62 +1,75 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import axios from 'axios';
 import './Products.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import queryString from 'query-string'
 // import Product from '../Product/Product'
 
 class Products extends Component {
-   constructor(props) {
-       super(props)
+    constructor(props) {
+        super(props)
 
-       this.state = {
-           products: []
+        this.state = {
+            products: []
 
-       }
-   }
-   
-
-   componentDidMount() {
-       window.onhashchange = this.getInitialProducts
-       this.getInitialProducts()
+        }
     }
-    
-    getInitialProducts = () => {
-        console.log(this.props.location)
-        const values = queryString.parse(this.props.location.search)
-        console.log(window)
-       axios.get(`/api/productsByCategory?category=${values.category}`).then(res => {
-          this.setState({
-             products: res.data
-          })
-      })
-        
-   }
 
-   render() {
-       var mappedItems = this.state.products.map((product, index) => {
-           return (
-            <div className='product-wrapper'>
-            <Link to={`/product/view/${product.product_id}`} style={{textDecoration: 'none'}} >
-               <div key={index}>
-                  <p className='info'>{product.title}</p>
-                  <img className='products-img' src={product.main_img} alt={product.description} style={{width:'300px', height:'300px'}} />
-                  <hr/>
-                  <p className='price'>Price: ${product.price}.00</p>
+
+    componentDidMount() {
+        window.onhashchange = this.getInitialProducts
+        this.getInitialProducts()
+    }
+
+    getInitialProducts = () => {
+        if (this.props.location.search) {
+            console.log(this.props.location)
+            const values = queryString.parse(this.props.location.search)
+            console.log(window)
+            axios.get(`/api/productsByCategory?category=${values.category}`).then(res => {
+                this.setState({
+                    products: res.data
+                })
+            })
+
+        } else {
+            this.getAllProducts()
+        }
+    }
+
+    getAllProducts = () => {
+        axios.get(`/api/products`).then(res => {
+            this.setState({
+                products: res.data
+            })
+        })
+    }
+
+    render() {
+        var mappedItems = this.state.products.map((product, index) => {
+            return (
+                <div className='product-wrapper'>
+                    <Link to={`/product/view/${product.product_id}`} style={{ textDecoration: 'none' }} >
+                        <div key={index}>
+                            <p className='info'>{product.title}</p>
+                            <img className='products-img' src={product.main_img} alt={product.description} style={{ width: '300px', height: '300px' }} />
+                            <hr />
+                            <p className='price'>Price: ${product.price}.00</p>
+                        </div>
+                    </Link>
                 </div>
-            </Link>
-            </div>
-           )
-       })
-       return(
-           <div >
+            )
+        })
+        return (
+            <div >
+
                 <p className='products-logo'>Products</p>
                 <div className='products-wrapper'>
-                {mappedItems}
+                    {mappedItems}
                 </div>
-           </div>
-       )
-   }
+            </div>
+        )
+    }
 
 }
 
